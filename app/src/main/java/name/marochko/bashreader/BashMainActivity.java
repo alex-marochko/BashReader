2d0ad8f0a356c6396ba68067017b1e3decae15fc
Alexander Marochko <marochko@gmail.com>
2015-11-15 23:31:15 +0200

    1st test commit.

---
+++ app/src/main/java/name/marochko/bashreader/BashMainActivity.java
@@ -0,0 +1,15 @@
+package name.marochko.bashreader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BashMainActivity extends AppCompatActivity {
+
+    private final static String TAG = "marinfo";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bash_main);
+    }
+}
