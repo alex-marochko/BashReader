19fe0043f9a597894998a21b05fba2c938481d3a
Alexander Marochko <marochko@gmail.com>
2015-11-16 01:10:50 +0200

    Added Jsoup lib and BashParse draft (without separate thread)

--- app/src/main/java/name/marochko/bashreader/BashMainActivity.java
+++ app/src/main/java/name/marochko/bashreader/BashMainActivity.java
@@ -1,15 +1,35 @@
 package name.marochko.bashreader;
 
+/*
+    Simple app to parse html.
+*/
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class BashMainActivity extends AppCompatActivity {
 
-    private final static String TAG = "marinfo";
+    private final static String LOG_TAG = "marinfo";
+
+    TextView textViewTemp;
+    Button buttonLoad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bash_main);
+
+        textViewTemp = (TextView)findViewById(R.id.textViewTemp);
+        buttonLoad = (Button)findViewById(R.id.buttonLoad);
+    }
+
+    public void onButtonLoadClick(View v){
+
+        textViewTemp.setText(BashParser.getPlainText());
+
     }
 }
