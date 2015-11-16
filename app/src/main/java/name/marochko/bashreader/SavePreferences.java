8732fd46ba7fc43b83d3c0be77b3ee483534ccd6
Alexander Marochko <marochko@gmail.com>
2015-11-16 18:21:12 +0200

    added comments

--- app/src/main/java/name/marochko/bashreader/SavePreferences.java
+++ app/src/main/java/name/marochko/bashreader/SavePreferences.java
@@ -13,8 +13,9 @@ import java.lang.reflect.Type;
 import java.util.List;
 
 /**
- * Created by Marochko on 16.11.2015.
+ * Just to save and restore list data.
  */
+
 public class SavePreferences {
 
     private static SharedPreferences.Editor preferencesEditor;
@@ -22,16 +23,12 @@ public class SavePreferences {
 
     public static void saveData(ListView listViewQuotes){
 
+//      converting list data to json and saving
         preferencesEditor = preferences.edit();
-
         Gson gson = new Gson();
-
         String[] array = getStringArray(listViewQuotes.getAdapter());
-
         String jsonString = gson.toJson(array);
-
         preferencesEditor.putString("list", jsonString);
-
         preferencesEditor.apply();
 
 
@@ -39,36 +36,26 @@ public class SavePreferences {
 
     public static void restoreData(ListView listViewQuotes, Activity activity){
 
+
+//      from Preferences to List<String>
         preferences = activity.getPreferences(activity.MODE_PRIVATE);
-
-
-
         Gson gson = new Gson();
         String jsonString;
-
         jsonString = preferences.getString("list", "");
-
         Type type = new TypeToken<List<String>>(){}.getType();
-
         List<String> stringList = gson.fromJson(jsonString, type);
 
 
-
+//      and now from List<String> to String[], then setting up the adapter
         if(stringList != null){
             String[] array = stringList.toArray(new String[1]);
             ArrayAdapter<String> adapter;
-
             adapter = new ArrayAdapter<>(activity, R.layout.quotes_list_item, array);
-
             listViewQuotes.setAdapter(adapter);
-
         }
-
-
-
-
     }
 
+//
     public static String[] getStringArray(ListAdapter adapter){
         String[] a = new String[adapter.getCount()];
 
@@ -77,6 +64,5 @@ public class SavePreferences {
 
         return a;
     }
-
 
 }
