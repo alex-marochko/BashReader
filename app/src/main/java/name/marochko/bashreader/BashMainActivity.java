86606e7c72fc53e9d876d308ba80602e5f974e56
Alexander Marochko <marochko@gmail.com>
2015-11-16 05:09:55 +0200

    Works pretty good

--- app/src/main/java/name/marochko/bashreader/BashMainActivity.java
+++ app/src/main/java/name/marochko/bashreader/BashMainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
@@ -15,7 +16,7 @@ public class BashMainActivity extends AppCompatActivity {
 
     private final static String LOG_TAG = "marinfo";
 
-    TextView textViewTemp;
+    ListView listViewQuotes;
     Button buttonLoad;
 
     @Override
@@ -23,13 +24,13 @@ public class BashMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bash_main);
 
-        textViewTemp = (TextView)findViewById(R.id.textViewTemp);
+        listViewQuotes = (ListView)findViewById(R.id.listViewQuotes);
         buttonLoad = (Button)findViewById(R.id.buttonLoad);
     }
 
     public void onButtonLoadClick(View v){
 
-        new BashParser(textViewTemp).execute();
+        new BashParser(listViewQuotes, this).execute();
 
     }
 }
--- app/src/main/java/name/marochko/bashreader/BashParser.java
+++ app/src/main/java/name/marochko/bashreader/BashParser.java
@@ -4,45 +4,66 @@ package name.marochko.bashreader;
  * Created by SDN on 16.11.2015.
  */
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
 public class BashParser extends AsyncTask<Void, Void, Void> {
 
-    private final static String LOG_TAG = "marinfo";
-    private final static String URL = "http://bash.im/random";
-    private final static int QUOTES_PER_PAGE = 50;
-    private final static String TAG_TO_SELECT = "div.text";
-    TextView textView;
-    private static String plainText = null;
+    private final String LOG_TAG = "marinfo";
+    private final String URL = "http://bash.im/random";
+    private final int QUOTES_PER_PAGE = 50;
+    private final String TAG_TO_SELECT = "div.text";
+    private ListView listView;
+    private String plainText = null;
+    private String[] strArray = new String[QUOTES_PER_PAGE];
+    private Context context;
+    ArrayAdapter<String> adapter;
 
-    BashParser(TextView textView){
-        this.textView = textView;
+    BashParser(ListView listView, Context context){
+        this.listView = listView;
+        this.context = context;
+
         Log.i(LOG_TAG, "BashParser created");
     }
 
-    public void getPlainText() {
+    public void getList() {
         Document doc;
+        Elements els;
         try {
             doc = Jsoup.connect(URL).get();
-            plainText = doc.text();
+
+            els = doc.select(TAG_TO_SELECT);
+
+            int i = 0;
+
+            for(Element el:els ){
+                el = el.html(el.html().replace("<br>", "zzzxxxccc"));
+                strArray[i++]=el.text().replace("zzzxxxccc", "\n");
+            }
+
+            adapter = new ArrayAdapter<>(context, R.layout.quotes_list_item, strArray);
+
+
         } catch (IOException ioe) {
             Log.e(LOG_TAG, "IO Exception: ", ioe);
         }
 
-        Log.i(LOG_TAG, "getPlainText() end");
+        Log.i(LOG_TAG, "getList() end");
     }
 
     @Override
     protected Void doInBackground(Void... params) {
-        getPlainText();
+        getList();
         Log.i(LOG_TAG, "doInBackground(Void... params) end");
         return null;
     }
@@ -50,7 +71,7 @@ public class BashParser extends AsyncTask<Void, Void, Void> {
     @Override
     protected void onPostExecute(Void result){
         super.onPostExecute(result);
-        textView.setText(plainText);
+        listView.setAdapter(adapter);
         Log.i(LOG_TAG, "onPostExecute(Void result) end");
     }
 }
